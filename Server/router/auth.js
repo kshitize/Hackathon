9abb827bdb765f0019135311855eb904d023a3e7
Kshitize Dimri <explorerkd@gmail.com>
2021-09-19 02:38:35 +0530

    Login, adding user functionality implemented

--- Server/router/auth.js
+++ Server/router/auth.js
@@ -5,9 +5,45 @@ require('../db/conn');
 const Passenger = require('../model/paxSchema');
 const User = require('../model/userSchema');
 
-router.get('/login', (req, res) => {
-    res.send("signin page");
+
+//add user route
+router.post('/adduser', (req, res) => {
+
+    const { username, password, usertype } = req.body;
+
+    const user = new User({ username, password, usertype });
+
+    user.save().then(() => {
+        res.status(201).json("user details saved to db");
+    }).catch((err) => res.status(202).json("failed to add pax details"));
+});
+
+// login route by admin or stakeholder
+router.post('/login', async(req, res) => {
+    try {
+        const { username, password, usertype } = req.body;
+        if (!username || !password || !usertype) {
+            return res.status(201).json("Enter login credentials properly");
+        }
+        const userLogin = await User.findOne({ username: username });
+        console.log(userLogin);
+        if (!userLogin) {
+            res.json("User not in db");
+        } else {
+            //res.json("Matching login username and password");
+            if (userLogin.username === username && userLogin.password === password) {
+                res.json("User Login successful");
+            } else {
+                res.json("User Login failed");
+            }
+        }
+
+    } catch (error) {
+        console.log("login route error");
+    }
 })
+
+// registeration route for passenger by admin
 router.post('/register', (req, res) => {
 
     const { name, email, phone, discounttype, expiryofcoupon, nextdateofjourney, uniquekey } = req.body;
